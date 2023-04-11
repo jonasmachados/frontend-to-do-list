f62b89515f4e8a9aa5b99c5e88178401b9b006f6
jonasmachados <jonasmachado.ti@gmail.com>
2023-04-11 09:24:05 -0300

    Added handleClick event to display list information when clicked

--- src/components/ToDoLists/index.js
+++ src/components/ToDoLists/index.js
@@ -3,9 +3,11 @@ import ToDoListService from "../../services/ToDoListService";
 import { format, parseISO } from "date-fns";
 import { BsCalendar2Plus } from "react-icons/bs";
 import "./styles.css";
+import { useNavigate } from 'react-router-dom';
 
 const ToDoLists = () => {
     const [toDoList, setToDoList] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         getAllToDoLists();
@@ -22,18 +24,22 @@ const ToDoLists = () => {
             });
     };
 
+    const handleClick = (toDoListID) => {
+        navigate('/project/' + toDoListID);
+    };
+
     return (
         <div className="container-to-do-lists">
             <h1>Visualize todas as suas tarefas em um só lugar e adicione novas facilmente com apenas um clique.</h1>
 
             <ol className="project-list">
-                <li className="to-do-list" key={toDoList.id}>
+                <li className="to-do-list" >
                     <span className="name">Novo projeto</span>
                     <span className="icon"><BsCalendar2Plus /></span>
                 </li>
 
                 {toDoList.map((toDoList) => (
-                    <li className="to-do-list" key={toDoList.id}>
+                    <li className="to-do-list" key={toDoList.id} onClick={() => handleClick(toDoList.id)} >
                         <span className="name">{toDoList.name}</span>
                         <span className="date">Iniciado em {format(parseISO(toDoList.dateInitial), 'dd/MM/yyyy')}</span>
                     </li>
