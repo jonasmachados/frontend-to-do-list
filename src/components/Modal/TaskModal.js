1ee47bdbbd7f8cf81fa74fad28f8e623040527aa
jonasmachados <jonasmachado.ti@gmail.com>
2023-05-18 10:06:18 -0300

    Add validation to task form in TaskModal component

--- src/components/Modal/TaskModal.js
+++ src/components/Modal/TaskModal.js
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from 'react';
 import ModalComponent from './ModalComponent';
 import TaskService from '../../services/TaskService';
 import ToDoListService from '../../services/ToDoListService'
+import { validateTask } from '../../validations/validateTask'
 
 const TaskModal = ({ show, handleClose, task, projectId, updateListTasks, listTasks, setListTasks }) => {
+
+
+    // State variables
     const [title, setTitle] = useState(task?.title || '');
     const [content, setContent] = useState(task?.content || '');
     const [taskStatus, setTaskStatus] = useState(task?.taskStatus || '');
+    const [titleError, setTitleError] = useState({ title: '' })
+    const [contentError, setContentError] = useState({ content: '' })
 
-    useEffect(() => {
-        setTitle(task?.title || '');
-        setContent(task?.content || '');
-        setTaskStatus(task?.taskStatus || 'NOT_STARTED');
-    }, [task]);
+    // Event handlers
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+        setTitleError({ ...titleError, title: '' });
+    };
 
-    const handleAddOrUpdateTask = () => {
+    const handleContentChange = (event) => {
+        setContent(event.target.value);
+        setContentError({ ...contentError, content: '' });
+    };
+
+    const handleAddOrUpdateTask = async () => {
         const taskData = {
             title,
             content,
@@ -22,35 +33,50 @@ const TaskModal = ({ show, handleClose, task, projectId, updateListTasks, listTa
             dateInitial: new Date().toISOString()
         };
 
-        if (task && task.id) {
-            TaskService.updateTask(task.id, taskData)
-              .then((response) => {
-                const updatedTasks = listTasks.map((t) => (t.id === response.data.id ? response.data : t));
-                const sortedTasks = updateListTasks(updatedTasks);
-                setListTasks(sortedTasks);
-                handleClose();
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          } else {
-            ToDoListService.addTaskToList(projectId, taskData)
-              .then((response) => {
-                const updatedTasks = [...listTasks, response.data];
-                const sortedTasks = updateListTasks(updatedTasks);
-                setListTasks(sortedTasks);
-                handleClose();
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          }
+        // Validate task data
+        const validationResult = await validateTask(taskData);
 
-        setTitle('');
-        setContent('');
-        setTaskStatus('NOT_STARTED');
-        handleClose();
+        if (validationResult.isValid) {
+            // Update task
+            if (task && task.id) {
+                TaskService.updateTask(task.id, taskData)
+                    .then((response) => {
+                        const updatedTasks = listTasks.map((t) => (t.id === response.data.id ? response.data : t));
+                        const sortedTasks = updateListTasks(updatedTasks);
+                        setListTasks(sortedTasks);
+                        handleClose();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+            // Add new task
+            else {
+                ToDoListService.addTaskToList(projectId, taskData)
+                    .then((response) => {
+                        const updatedTasks = [...listTasks, response.data];
+                        const sortedTasks = updateListTasks(updatedTasks);
+                        setListTasks(sortedTasks);
+                        handleClose();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        } else {
+            setTitleError({ ...titleError, title: validationResult.errors });
+            setContentError({ ...contentError, content: validationResult.errors });
+        }
+
     };
+
+    // Update state variables when task prop changes
+    useEffect(() => {
+        setTitle(task?.title || '');
+        setContent(task?.content || '');
+        setTaskStatus(task?.taskStatus || 'NOT_STARTED');
+    }, [task]);
+
 
     return (
         <ModalComponent
@@ -60,27 +86,33 @@ const TaskModal = ({ show, handleClose, task, projectId, updateListTasks, listTa
             title={task && task.id ? "Atualizar Tarefa" : "Nova Tarefa"}
             body={
                 <div>
+                    {/* Task title input field */}
                     <div>
                         <input
                             type="text"
                             placeholder="Título"
                             name="title"
                             value={title}
-                            onChange={(e) => setTitle(e.target.value)}
+                            onChange={handleTitleChange}
                         />
+                        {titleError.title && <p style={{ color: "red", fontSize: "20px", margin: "0 0 30px 0" }}>{titleError.title.title}</p>}
                     </div>
 
+                    {/* Task content input field */}
                     <div>
                         <input
                             type="text"
                             placeholder="Conteúdo"
                             name="content"
                             value={content}
-                            onChange={(e) => setContent(e.target.value)}
+                            onChange={handleContentChange}
                         >
                         </input>
+                        {contentError.content && <p style={{ color: "red", fontSize: "20px", margin: "0 0 30px 0" }}>{contentError.content.content}</p>}
+
                     </div>
 
+                    {/* Task status select menu */}
                     <div>
                         <select
                             name="taskStatus"
