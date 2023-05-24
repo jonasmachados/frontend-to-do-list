926f751eaf63b92e89dfd6603e5799f2d807c377
jonasmachados <jonasmachado.ti@gmail.com>
2023-05-24 17:14:38 -0300

    Refactor URL variable to use constant for Netlify deployment

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TASK_API_BASE_URL = "http://localhost:8080/task";
+const TASK_API_BASE_URL = process.env.TASK_API_BASE_URL ?? "http://localhost:8080/task";
 
 class TaskService {
 
